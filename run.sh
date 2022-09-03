330c9e240531bfefe5ae17678d5b2ffa9f4ca1b0
Luis <luis@MacBook-Pro.local>
2022-09-03 13:53:12 -0500

    Show logs

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ docker stop api-rest || true && docker rm api-rest
 #Run Container Application
 docker run -itd --name api-rest -p 3000:3000 lzapienmx/miappcdk-ago22:1.0
 
+sleep 20
+docker logs api-rest
